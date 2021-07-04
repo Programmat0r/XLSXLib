51ece74f705ccbaae164798183cdbebef98c8680
nlang <nicolas.lang@nl-it.de>
2021-07-04 14:59:30 +0200

    Added Workbook and Worksheet class.

---
+++ XLSXLib/Workbook.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XLSXLib
+{
+    public class Workbook
+    {
+        public Byte[] File { get; set; }
+        public String Name { get; }
+        public Worksheet[] Worksheets { get; }
+
+        public Workbook()
+        {
+
+        }
+
+        public Workbook(Byte[] File)
+        {
+            if (File != null)
+            {
+                this.File = File;
+            } else
+            {
+                throw new FileLoadException("Given file is empty.");
+            }
+        }
+    }
+
+    
+
+
+}
